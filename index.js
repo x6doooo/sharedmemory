95a5881f47dbc0630f6da89266a15e4182b99b5b
x6doooo <x6doooo@gmail.com>
2014-03-24 08:55:33 +0800

    调整结构

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+* @project SharedMemory
+* @author Dx.Yang <x6doooo@gmail.com>
+* @version 0.0.6
+* @license See LICENSE-MIT file included in this distribution.
+*/
+
+
+module.exports = require('./lib/sharedmemory');
