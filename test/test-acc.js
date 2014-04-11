374c270802486f361fb1c54de807bb714c916514
x6doooo <x6doooo@gmail.com>
2014-04-11 15:37:08 +0800

    增加并发测试

---
+++ test/test-acc.js
@@ -0,0 +1,38 @@
+var initSharedMemory = require('../lib/sharedmemory').init;
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+var http = require('http');
+var sharedMemoryController = initSharedMemory();
+
+
+if (cluster.isMaster) {
+
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    setInterval(function(){
+        console.log('共享内存中count值：' + sharedMemoryController.__sharedMemory__.get('count'));
+    }, 10 * 1000);
+
+
+} else {
+
+    var worker_count = 0;
+
+    http.createServer(function(req,res){
+
+        worker_count += 1;
+        
+        sharedMemoryController.plusOne('count');
+
+        res.end();
+
+    }).listen(1337);
+
+    setInterval(function(){
+        console.log('worker-' + cluster.worker.id + '响应请求次数：' +  worker_count);
+    }, 10 * 1000);
+
+}
+
